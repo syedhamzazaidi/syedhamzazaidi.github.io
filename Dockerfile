fd4427c7d6dc5f7b15c379b0e8adff746cd3b443
syedhamzazaidi <syedhamzazaidi5@gmail.com>
2022-01-14 00:22:01 +0530

    change Docker workdir

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ruby:2.7
 RUN gem install jekyll bundler
-WORKDIR /src/blog
+WORKDIR /src
 ENTRYPOINT bundler update && bundle exec jekyll serve --host 0.0.0.0 --config _config.yml
--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 docker build -t jekyll-env -f Dockerfile .
-docker run --name jekyll-env --mount type=bind,source=$(pwd),target=/src/blog -p 4000:4000 -it jekyll-env
+docker run --name jekyll-env --mount type=bind,source=$(pwd),target=/src -p 4000:4000 -it jekyll-env
